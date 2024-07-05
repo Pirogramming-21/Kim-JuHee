418d4c4945cf6f4c17ec1e25b1d15165d7c7ca6d
juhee <6820juhee@gmail.com>
2024-07-05 13:54:09 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,7 +1,7 @@
 num = 0
 turn = 'A'
 
-while num < 31:     # 5단계, 6단계 모두 만족
+while num < 31:     
     while True:
         try:
             n = int(input(f"player{turn}, 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
@@ -15,10 +15,6 @@ while num < 31:     # 5단계, 6단계 모두 만족
     for i in range(n):
         num += 1
         print(f"player{turn} : {num}")
-    
-    if num >= 31:
-        print(f"player{turn} win!")
-        break
     
     if turn == 'A':
         turn = 'B'
